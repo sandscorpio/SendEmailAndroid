9ca7f750635aa10a09f6e64fb2e1cf60077af9da
amit aggarwal <amit@mangobird.com>
2015-04-25 16:22:32 -0700

    wire up ui

--- src/com/codingchallenge/sendemailrobust/MainActivity.java
+++ src/com/codingchallenge/sendemailrobust/MainActivity.java
@@ -1,17 +1,41 @@
 package com.codingchallenge.sendemailrobust;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+	
+	private EditText mTxtFrom;
+	private EditText mTxtTo;
+	private EditText mTxtSubject;
+	private EditText mTxtBody;
+	private Button mBtnSend;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mTxtFrom = (EditText) findViewById (R.id.txtFrom);
+		mTxtTo = (EditText) findViewById (R.id.txtTo);
+		mTxtSubject = (EditText) findViewById (R.id.txtSubject);
+		mTxtBody = (EditText) findViewById (R.id.txtBody);
+		mBtnSend = (Button) findViewById (R.id.btnSend);
+		mBtnSend.setOnClickListener(btnSendClicked);
 	}
+	
+	private OnClickListener btnSendClicked = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			//send email
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
